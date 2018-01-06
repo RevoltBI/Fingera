21b0b0117fafeaee4d6a50dff482503bdea6b0f2
RevoltBI <34216118+RevoltBI@users.noreply.github.com>
2018-01-06 17:25:39 +0100

    Update main.R
    parallelize the operation

--- main.R
+++ main.R
@@ -2,11 +2,10 @@
 #Install the libraries
 library(httr)
 library(data.table)
-install.packages("dtplyr")
 library(dtplyr)
 library(xml2)
-install.packages("purrr")
 library(purrr)
+library(doParallel)
 
 #=======BASIC INFO ABOUT THE Fingera EXTRACTOR========#
 
@@ -22,37 +21,15 @@ password<-app$getParameters()$`#password`
 user<-app$getParameters()$user
 # start_date<-app$getParameters()$from
 # end_date<-app$getParameters()$to
-url<-app$getParameters()$url
+api<-app$getParameters()$api_url
 
 ##Catch config errors
 
 if(is.null(user)) stop("enter your username in the user config field")
 if(is.null(password)) stop("enter your password in the #password config field")
-# if(is.null(start_date)|is.null(end_date)) {
-#   warning("You entered an invalid date range, the extract will extract data from last 1 day")
-#   end_date<-Sys.Date()
-#   start_date<-end_date-1
-#   end_date<-as.POSIXct(end_date)
-#   start_date<-as.POSIXct(start_date)
-# }
-# from<-format(as.Date(start_date), "%d.%m.%Y")
-# to<-format(as.Date(end_date), "%d.%m.%Y")
 
-
-# List of available endpoints
-# day_schedules
-# event_categories
-# fingera_stations
-# groups
-# local_settings
-# settings
-# terminals
-# time_logs
-# user_accounts
-# users
 
 ##Now process the request
-
 
 getStats<-function(api,endpoint){
   
@@ -80,8 +57,7 @@ getTimelogs<-function(api,user_id){
     "user_id"=user_id
   )
   
-  r <-
-    RETRY(
+r<-RETRY(
       "GET",
       url,
       config = authenticate(user,password),
@@ -90,16 +66,23 @@ getTimelogs<-function(api,user_id){
       pause_cap = 10,
       query = args
     )
-  
+
   res<-content(r,"text",encoding = "UTF-8")%>%fromJSON
   
 }
 
 #iterate over users
-user_ids<-getStats(url,"users")%>%filter(as.Date(substr(created_at,1,10))<as.Date(end_date))%>%select(id)%>%.[,1]
+users<-getStats(api,"users")
 
-test<-user_ids[1:10]
+user_ids<-users%>%select(id)%>%.[,1]
 
-res<-map_df(user_ids,function(x){getTimelogs(url,x)})
+registerDoParallel(cores=detectCores()-1)
+
+res<-foreach(i=user_ids,.combine=bind_rows,.multicombine = TRUE, .errorhandling="remove") %dopar% {
+
+getTimelogs(api,i)
+
+}
 
 write_csv(res,"out/tables/time_logs.csv")
+write_csv(res,"out/tables/users.csv")
